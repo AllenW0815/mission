80b6a3ed6c48619ad2f7822ee23d998d3d829d05
Allen <ro81516@gmail.com>
2021-04-04 17:28:31 +0800

    Promise 處理非同步

--- mission_29/countdown.js
+++ mission_29/countdown.js
@@ -4,6 +4,9 @@ const hour = document.querySelector('.hour h2')
 const minute = document.querySelector('.minute h2')
 const second = document.querySelector('.second h2')
 
+const main = document.querySelector('main')
+const wrapper = document.querySelector('.wrapper')
+
 // const currentTime = new Date().getTime()
 const targetTime = new Date("2022/01/01 00:00:00").getTime()
 
@@ -23,7 +26,7 @@ const updateTime = () =>{
     let m = Math.floor(timeSub/1000/60)%60
     let s = Math.floor(timeSub/1000)%60
 
-    console.log(d,h,m,s);
+    // console.log(d,h,m,s);
     // console.log(Math.ceil(d));
     // console.log(Math.floor(d));
 
@@ -34,3 +37,14 @@ const updateTime = () =>{
 }
 
 setInterval(updateTime,1000)
+
+const afterLoading = new Promise((res,rej)=>{
+    setTimeout(function(){
+        wrapper.remove()
+        res()
+    },2500)
+})
+
+afterLoading.then(()=>{
+   main.classList.remove('hidden')
+})
