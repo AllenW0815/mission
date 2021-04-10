87a4323497330e1c1459ce41b1776a04738906ab
Allen <ro81516@gmail.com>
2021-04-10 15:11:16 +0800

    完成進度條動畫以及點擊跳轉

--- mission_26/music_player.js
+++ mission_26/music_player.js
@@ -8,10 +8,13 @@ const img = document.querySelector('.img-container img')
 const prevBtn = document.querySelector('#prev')
 const playBtn = document.querySelector('#play')
 const nextBtn = document.querySelector('#next')
-play
+
+const progressArea = document.querySelector('.progress-area')
+const progress = document.querySelector('.progress')
+
 // song list
-const songs = ['Lost_in_paradise','Desperado','Wild_side']
-let songIndex = 2
+const songs = ['Lost_in_paradise','Desperado','Wild_side','山雀','Yuve_Yuve_Yu']
+let songIndex = 0
 
 loadingSong(songs[songIndex])
 
@@ -43,17 +46,49 @@ function musicPlay() {
 
 playBtn.addEventListener('click',musicPlay)
 
-function musicPrev () {
-    pauseMode()
+
+function musicPrev() {
     songIndex > 0 ? songIndex-- : songIndex
     loadingSong(songs[songIndex])
+    playingMode()
 }
-function musicNext () {
-    pauseMode()
+function musicNext() {
     songIndex < songs.length -1  ? songIndex++ : songIndex
     loadingSong(songs[songIndex])
+    playingMode()
 }
 
-
-prevBtn.addEventListener('click',musicPrev)
+prevBtn.addEventListener('click',musicPrev) 
 nextBtn.addEventListener('click',musicNext)
+
+// for progress
+
+function updateProgress(e) {
+    // console.log(e);
+    // console.log(e.srcElement);
+    // 待研究
+    const {duration,currentTime} = e.srcElement
+    // console.log({duration,currentTime});
+    const progressPercent = (currentTime / duration) * 100
+    // console.log(progressPercent);
+    progress.style.width = `${progressPercent}%`
+}
+
+audio.addEventListener('timeupdate',updateProgress)
+
+function setProgress(e) {
+    // div的長度
+    const width = this.clientWidth
+    console.log(width);
+    // 點擊位置的長度
+    const x = e.offsetX
+    console.log(x);
+    // 音樂長度
+    const duration =  audio.duration
+    console.log(duration);
+
+    audio.currentTime = ( x / width ) * duration
+}
+
+progressArea.addEventListener('click',setProgress)
+
