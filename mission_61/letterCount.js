514f8c3403613d295f61c010e8c8681239f86e70
Allen <ro81516@gmail.com>
2021-05-03 23:54:31 +0800

    初步的樣式 初步功能

--- mission_61/letterCount.js
+++ mission_61/letterCount.js
@@ -1,15 +1,17 @@
 // DOM
 
 const monitor = document.querySelector('#monitor')
-const result = document.querySelector('.result')
+const result = document.querySelector('.result h2')
 
 // data
 
 let answer = null
 
+console.log(result);
+
 function counting () {
     answer = monitor.value.length
-    result.innerHTML = answer
+    result.innerText = answer
 }
 
 monitor.addEventListener('input',counting)
